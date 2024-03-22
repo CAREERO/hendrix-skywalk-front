6d7596b4d60535bbc1b9e31631e182066a86728f
John Rodney Bargayo <johnrodney.bargayo@gmail.com>
2024-03-22 22:29:11 +0800

    updated the api connection

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
-// Remove the apiUrl variable if it's not used elsewhere
+const baseURL = process.env.NODE_ENV === 'production' 
+  ? process.env.REACT_APP_API_TARGET_PROD || 'http://54.146.118.222:8000' // Fallback URL
+  : process.env.REACT_APP_API_TARGET_LOCAL || 'http://localhost:8000'; // Fallback URL
+
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_TARGET_PROD : process.env.REACT_APP_API_TARGET_LOCAL,
+  baseURL: baseURL
 });
 
 export default api;
